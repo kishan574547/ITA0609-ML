88a9fe7a112c98c7e25859306fda45f1ad37cbc5
KISHAN REDDY .N <192424404.simats@saveetha.com>
2026-07-27 13:43:45 +0530

    Delete Password Strength Checker (1).py

--- Password Strength Checker (1).py
+++
@@ -1,8 +0,0 @@
-import re
-pwd = input("Enter password: ")
-
-if (len(pwd) >= 8 and re.search("[A-Z]", pwd) and re.search("[a-z]", pwd)
-    and re.search("[0-9]", pwd) and re.search("[@#$%^&*!]", pwd)):
-    print("Strong Password")
-else:
-    print("Weak Password")
